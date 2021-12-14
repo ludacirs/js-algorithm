5b98fd6efb2d301cd0aaf93c4feb2dea6181eeff
ludacirs <ludacirs@naver.com>
2021-12-14 16:26:01 +0900

    update 20211214

--- 백준/입력 코드.js
+++ 백준/입력 코드.js
@@ -6,3 +6,21 @@ const input = fs.readFileSync("/dev/stdin").toString().split(' ')
 const input = require("fs").readFileSync("/dev/stdin").toString().trim()
 
 const N = Number(require("fs").readFileSync('/dev/stdin').toString().trim());
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input = ''
+rl.on('line', function(line) {
+    input += line+'\n';
+}).on('close',function (){
+    solution(input.slice(0,-1))
+    process.exit();
+});
+
+function solution(input){
+
+}
